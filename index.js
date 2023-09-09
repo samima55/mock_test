03b61dfaf1fbc7dc7f59fdd4016a936ce8675374
Samima Hassan <samimahassan55@gmail.com>
2023-09-09 16:29:57 +0200

    index.js added

---
+++ index.js
@@ -0,0 +1,27 @@
+// Wait for the DOM to be ready
+document.addEventListener("DOMContentLoaded", function () {
+    // Get references to elements
+    const submitButton = document.getElementById("submit-button");
+    const ratingForm = document.getElementById("rating-form");
+    const thankYouContainer = document.querySelector(".thank-you-container");
+    const chosenNumber = document.getElementById("chosen-number");
+
+    // Add a click event listener to the Submit button
+    submitButton.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent form submission
+        
+        // Find the checked radio button
+        const checkedRadio = ratingForm.querySelector('input[name="rating"]:checked');
+
+        // If a radio button is checked, proceed
+        if (checkedRadio) {
+            const value = checkedRadio.value; // Get the selected rating value
+            thankYouContainer.style.display = "block"; // Show the Thank You container
+            ratingForm.style.display = "none"; // Hide the rating form
+            thankYouContainer.style.textAlign = "center"; // Center-align text
+
+            // Update the chosen number in the Thank You message
+            chosenNumber.textContent = value;
+        }
+    });
+});
